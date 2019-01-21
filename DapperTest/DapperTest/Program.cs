6e3eb4f836480247ea5bfcc95077218e082a936a
VyseMACHINE <40603631+VyseMACHINE@users.noreply.github.com>
2019-01-21 22:08:58 +0600

    Add files via upload

---
+++ DapperTest/DapperTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Configuration;
+using Dapper;
+namespace DapperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string insertSql = "Insert into people values (@fullName, @phoneNumber, @email)";
+            string selectSql = "select * from people";
+            var providerName = ConfigurationManager.ConnectionStrings["DapperConnection"].ProviderName;
+            var providerFactory = DbProviderFactories.GetFactory(providerName);
+            using (var connection = providerFactory.CreateConnection())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["DapperConnection"].ConnectionString;
+                var person = new Person
+                {
+                   
+                    FullName = "Ivan",
+                    PhoneNumber = "+77018882628",
+                    Email = "Ivan@gmail.com"
+                };
+                
+              var result = connection.Execute(insertSql, person);
+               
+                /* connection.Execute(insertSql, new
+                 {
+                     person.Id,
+                     person.FullName,
+                     person.PhoneNumber,
+                     person.Email });
+                }*/
+
+                if (result != 1) Console.WriteLine("Error!");
+                else
+                {
+                    var people = connection.Query<Person>(selectSql);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ HomeTask/HomeTask/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HomeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string insertPlayer = "Insert into Player values (@Name, @Number, @TeamId)";
+            string insertTeam = "Insert into Team values (@Id, @Name)";
+
+            string selectPlayer = "select * from Team";
+            string selectTeam = "select * from people";
+            var providerName = ConfigurationManager.ConnectionStrings["DapperConnection"].ProviderName;
+            var providerFactory = DbProviderFactories.GetFactory(providerName);
+            using (var connection = providerFactory.CreateConnection())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["DapperConnection"].ConnectionString;
+                var player = new Player
+                {
+                  
+                    Name = "Jehnsen Ackles",
+                    Number = 10,
+                    TeamId = 1
+
+                };
+
+                var team = new Team
+                {
+                    Id = 1,
+                    Name = "Winners"                  
+                };
+
+                var result = connection.Execute(insertPlayer, player);
+                var result1 = connection.Execute(insertTeam, team);
+
+                /* connection.Execute(insertSql, new
+                 {
+                     person.Id,
+                     person.FullName,
+                     person.PhoneNumber,
+                     person.Email });
+                }*/
+
+                var people1 = connection.Query<Team>(selectPlayer);
+                if (result != 1) Console.WriteLine("Error!");
+                else
+                {
+                    var people = connection.Query<Player>(selectPlayer);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
